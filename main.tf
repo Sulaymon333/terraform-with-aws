d63f0333b55cff2dfaf67b4db1533a8abedc1e02
sulaymon333 <tjsulaymon@gmail.com>
2022-08-29 01:02:23 +0300

    added more resources - subnet, ig, sg, route table

--- main.tf
+++ main.tf
@@ -7,3 +7,61 @@ resource "aws_vpc" "sd_vpc" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "sd_public_subnet" {
+  vpc_id                  = aws_vpc.sd_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-north-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "sd_internet_gateway" {
+  vpc_id = aws_vpc.sd_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "sd_public_rt" {
+  vpc_id = aws_vpc.sd_vpc.id
+
+  tags = {
+    Name = "dev-public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.sd_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.sd_internet_gateway.id
+}
+
+resource "aws_route_table_association" "sd_public_assoc" {
+  subnet_id      = aws_subnet.sd_public_subnet.id
+  route_table_id = aws_route_table.sd_public_rt.id
+}
+
+resource "aws_security_group" "sd_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.sd_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
